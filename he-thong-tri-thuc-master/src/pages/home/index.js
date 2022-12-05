c5a014ea91a19b221ecec8f5e6cebeabdd7ae1b0
huyhq <hung.nguyenmanh@powergatesoftware.com>
2022-12-05 23:08:58 +0700

    call-api

--- he-thong-tri-thuc-master/src/pages/home/index.js
+++ he-thong-tri-thuc-master/src/pages/home/index.js
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import image from '../../assets/image/anh1.png'
+import { Modal, Button } from 'antd';
 
 const Home = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
   return (
     <div>
       <header className="masthead">
@@ -13,30 +27,11 @@ const Home = () => {
               <hr className="divider" />
             </div>
             <div className="col-lg-8 align-self-baseline">
-              <Link className="btn btn-primary btn-xl" to="/system">Bắt đầu</Link>
+              <Link className="btn btn-primary btn-xl" to="/system">Bắt đầuu</Link>
             </div>
           </div>
         </div>
       </header>
-      {/* <section class="page-section" id="services">
-        <div class="container px-4 px-lg-5">
-          <h2 class="text-center mt-0">Tri thức của hệ thống</h2>
-          <hr class="divider" />
-          <div class="row gx-4 gx-lg-5">
-            <img style={{ height: 800, }} src={image} />
-            <p class="mt-4">Hệ thống giải quyết theo 2 phương diện:</p>
-            <p class="mt-2 ml-2">- Bộ điều khiển đánh lái mô phỏng các hành vi sau của con người:</p>
-            <p class="ml-4">+ lái chiếc xe về phía mục tiêu (đi thẳng hoặc rẽ trái, phải).</p>
-            <p class="ml-4">+ lái xe quanh bất kỳ chướng ngại vật nào để tránh va chạm.</p>
-            <p class="ml-4">+ điều khiển xe dừng lại ở một hướng mong muốn.</p>
-            <p class="mt-2 ml-2">- Bộ điều khiển vận tốc mô phỏng các hành vi sau của con người:</p>
-            <p class="ml-4">+ khởi động xe từ điểm dừng hoàn toàn và dừng xe khi xe đến mục tiêu.</p>
-            <p class="ml-4">+ giảm tốc độ xe khi nó đến gần chướng ngại vật và tăng tốc khi nó vượt qua chướng ngại
-              vật.</p>
-            <p class="ml-4">+ giảm tốc độ xe khi đi vào khúc cua, đoạn cần rẽ.</p>
-          </div>
-        </div>
-      </section> */}
     </div>
   );
 };
